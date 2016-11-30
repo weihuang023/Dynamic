15a593bfefbadf48cab12e15d7a931ed4dd64314
weihuang023 <weih81888@gmail.com>
2016-11-29 21:44:46 -0500

    add cutting rob

--- src/CuttingRob.java
+++ src/CuttingRob.java
@@ -1,34 +1,5 @@
-<<<<<<< HEAD
-public class CuttingRob {
 
-	public static int profitDP(int[] value, int length) {
-		int[] solution = new int[length + 1];
-		solution[0] = 0;
-		int z = 1;
-		for (int i = 1; i <= length; i++) {
-			int max = -1;
-			for (int j = 0; j < i; j++) {
-				max = Math.max(max, value[j] + solution[i - (j + 1)]);
-				solution[i] = max;
-				System.out.println("Iteration"+z+": for length = " +i+" Profit = "+max);
-				z++;
-			}
-		}
-		return solution[length];
-	}
 
-	public static void main(String[] args) {
-			
-		int[] value = { 2, 3, 7, 8, 9 };
-		int len = 5;
-		
-		System.out.println("Max profit for length = " +len+ " profit = "
-				+ profitDP(value, len));
-
-	}
-
-}
-=======
 import java.util.Scanner;
 
 class CuttingRob{
@@ -101,4 +72,3 @@ class CuttingRob{
 	}
 }
 
->>>>>>> 305daa90ea9044ab1ff4d08addf27a9679862df1
